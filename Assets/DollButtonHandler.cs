2b4633a31491ff1032adefc5b306a523b41cde6f
aainsrm@gmail.com <aainsrm@gmail.com>
2019-01-04 20:35:23 +0330

    fourth wall zoom in and zoom out mechanic implemented, all walls tested

---
+++ Assets/DollButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DollButtonHandler : MonoBehaviour {
+
+    public GameObject DollCloseUp;
+    // Use this for initialization
+    void Start()
+    {
+        Button tableButton = gameObject.GetComponent<Button>();
+        tableButton.onClick.AddListener(ZoomOnDoll);
+    }
+
+    private void ZoomOnDoll()
+    {
+
+        Instantiate(DollCloseUp);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ZoomButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ZoomButtonHandler : MonoBehaviour {
+    public GameObject closeUpObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button tableButton = gameObject.GetComponent<Button>();
+        tableButton.onClick.AddListener(ZoomIn);
+    }
+
+    private void ZoomIn()
+    {
+
+        Instantiate(closeUpObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
