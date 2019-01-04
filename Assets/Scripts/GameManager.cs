88c0bbf3151c87becdfdacbefc858ec00b8e769a
aainsrm@gmail.com <aainsrm@gmail.com>
2019-01-04 16:42:52 +0330

    first wall zoom in and zoom out mechanic implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> walls = new List<GameObject>(4);
+
+    // Use this for initialization
+    void Awake () {
+        Instantiate(walls[0]);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HangerButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HangerButtonHandler : MonoBehaviour {
+
+    public GameObject hangerCloseShot;
+    
+
+	// Use this for initialization
+	void Start () {
+        Button hangerButton = gameObject.GetComponent<Button>();
+        hangerButton.onClick.AddListener(ZoomOnHanger);
+	}
+
+    private void ZoomOnHanger()
+    { 
+        Instantiate(hangerCloseShot);
+    }
+
+   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
