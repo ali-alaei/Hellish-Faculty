fb36f62fdbc6627bb8d5b590de94b316f37293c5
ali <aainsrm@gmail.com>
2019-01-28 01:21:08 +0330

    prototype changes added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Awake () {
-        Instantiate(walls[2]);
+        Instantiate(walls[0]);
         
 		
 	}
---
+++ Assets/Scripts/OpenDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenDrawer : MonoBehaviour {
+    public List<Sprite> drawerImages;
+    private Button[] drawerButtons;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < GetComponentsInChildren<Button>().Length; i++)
+        {
+            drawerButtons = GetComponentsInChildren<Button>();
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
