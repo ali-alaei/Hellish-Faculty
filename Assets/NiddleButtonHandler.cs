57f52fe6635ba2e89e69fe870371f5de62f04707
ali <aainsrm@gmail.com>
2019-01-29 22:37:22 +0330

    inventory basics implemented, still in progress

---
+++ Assets/NiddleButtonHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NiddleButtonHandler : MonoBehaviour
+{
+
+	public InventoryManager gameInventory;
+
+	private bool needleAdded;
+	
+	// Use this for initialization
+	void Start () {
+		Button objectButton = gameObject.GetComponent<Button>();
+		objectButton.onClick.AddListener(AddToInventory);
+		needleAdded = false;
+	}
+
+	void AddToInventory()
+	{
+		if (!needleAdded)
+		{
+			gameInventory.AddToInventoryList(gameObject);
+			needleAdded = true;
+			print("Added");
+		}
+	}
+	
+}
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,13 +6,23 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour {
 
 	// Use this for initialization
-	public List<GameObject> InventoryList; 
+	private List<GameObject> InventoryList = new List<GameObject>();
+
+	public void AddToInventoryList(GameObject clickedGameObject)
+	{
+		InventoryList.Add(clickedGameObject);
+		print("size = " + InventoryList.Count);
+		
+	}
+	
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		print("size = " + InventoryList.Count);
+
 		
 	}
 }
